0e9f805ebb82f228ff8d8249ce3d889772cdc409
Thắng Nguyễn <Thng Nguyn>
2017-07-25 16:25:01 +0700

    finishing

--- app-client/common/directives/navigation/navigation.controller.js
+++ app-client/common/directives/navigation/navigation.controller.js
@@ -7,7 +7,16 @@
 	function navigationCtrl($route,$location,authentication) {
 		var vm = this;
 		vm.currentPath = $location.path();
-
+		$('nav').find('.active').removeClass('active');
+		switch (vm.currentPath) {
+			case '/':
+				$("#home").addClass('active');
+				break;
+			case '/login': case '/register': case '/about':
+				var id = vm.currentPath.replace('/','#');
+				$(id).addClass('active');
+				break;
+		}
 		vm.isLoggedIn = authentication.isLoggedIn();
 		vm.currentUser = authentication.currentUser();
 
